ec2e64ef512a3ba4d5b8f74999b215dc06927540
Linda Xiao <linda.xiao.20@dartmouth.edu>
2018-04-14 20:54:59 -0400

    get_body() edits

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ Office.initialize = function (reason) {
 function loadItemProps(item) {
   // Get the table body element
   var tbody = $('.prop-table');
+  var text = getBody();
 
   // Add a row to the table for each message property
   // tbody.append(makeTableRow("Id", item.itemId));
@@ -24,7 +25,8 @@ function loadItemProps(item) {
   tbody.append(makeTableRow("Date: "));
   tbody.append(makeTableRow("Time: "));
   tbody.append(makeTableRow("Location: "));
-
+  tbody.append(makeTableRow("XXX: ", text));
+  
 }
 
 function makeTableRow(name, value) {
@@ -61,49 +63,15 @@ function loadEntities() {
   }
 }
 
-=======
-(function () {
-  
-  // The initialize function must be run each time a new page is loaded
-  Office.initialize = function (reason) {
-    $(document).ready(function () {
-      loadItemProps(Office.context.mailbox.item);
-      getBody();
-    });
-  };
-  
-  function loadItemProps(item) {
-    // Get the table body element
-    var tbody = $('.prop-table');
-    
-    // Add a row to the table for each message property
-    // tbody.append(makeTableRow("Id", item.itemId));
-    tbody.append(makeTableRow("Event:", item.subject));
-    // tbody.append(makeTableRow("Message Id", item.internetMessageId));
-    tbody.append(makeTableRow("With", item.from.displayName));
-    tbody.append(makeTableRow("Date: "));
-    tbody.append(makeTableRow("Time: "));
-    tbody.append(makeTableRow("Location: "));
-    
-  }
-  
-  function makeTableRow(name, value) {
-    return $("<tr><td><strong>" + name + 
-    "</strong></td><td class=\"prop-val\"><code>" +
-    value + "</code></td></tr>");
-  }
-  
 function getBody(){
-    var _item = Office.context.mailbox.item;
-    var body = _item.body;
-    
-    body.getAsync(Office.CoercionType.Text, function (asyncResult) {
-      if (asyncResult.status !== Office.AsyncResultStatus.Succeeded){
-        //Handle Error
-        } else {
-        showDataDialog('Body', asyncResult.value.trim());
-      }
-    });
-  }
+  var _item = Office.context.mailbox.item;
+  var body = _item.body;
   
-})();
+  body.getAsync(Office.CoercionType.Text, function (asyncResult) {
+    if (asyncResult.status !== Office.AsyncResultStatus.Succeeded){
+      //Handle Error
+    } else {
+      return asyncResult.value.trim();
+    }
+  });
+}
