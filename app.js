6c8e82b1c6abf335dd8a62ce413f736a977b7b83
Linda Xiao <linda.xiao.20@dartmouth.edu>
2018-04-14 20:44:21 -0400

    Playing with regular expressions.

--- app.js
+++ app.js
@@ -5,34 +5,59 @@
 
 'use strict';
 
-(function () {
-
   // The initialize function must be run each time a new page is loaded
-  Office.initialize = function (reason) {
-    $(document).ready(function () {
-      loadItemProps(Office.context.mailbox.item);
-    });
-  };
+Office.initialize = function (reason) {
+  $(document).ready(function () {
+    loadItemProps(Office.context.mailbox.item);
+  });
+}
 
-  function loadItemProps(item) {
-    // Get the table body element
-    var tbody = $('.prop-table');
+function loadItemProps(item) {
+  // Get the table body element
+  var tbody = $('.prop-table');
 
-    // Add a row to the table for each message property
-    // tbody.append(makeTableRow("Id", item.itemId));
-    tbody.append(makeTableRow("Event:", item.subject));
-    // tbody.append(makeTableRow("Message Id", item.internetMessageId));
-    tbody.append(makeTableRow("With", item.from.displayName));
-    tbody.append(makeTableRow("Date: "));
-    tbody.append(makeTableRow("Time: "));
-    tbody.append(makeTableRow("Location: "));
+  // Add a row to the table for each message property
+  // tbody.append(makeTableRow("Id", item.itemId));
+  tbody.append(makeTableRow("Event:", item.subject));
+  // tbody.append(makeTableRow("Message Id", item.internetMessageId));
+  tbody.append(makeTableRow("With", item.from.displayName));
+  tbody.append(makeTableRow("Date: "));
+  tbody.append(makeTableRow("Time: "));
+  tbody.append(makeTableRow("Location: "));
 
+}
+
+function makeTableRow(name, value) {
+  return $("<tr><td><strong>" + name + 
+    "</strong></td><td class=\"prop-val\"><code>" +
+    value + "</code></td></tr>");
+}
+
+function loadEntities() {
+  // getSelectedRegExMatches is in preview, so need to test for it
+  if (Office.context.mailbox.item.getSelectedRegExMatches !== undefined) {
+    var selectedMatches = Office.context.mailbox.item.getSelectedRegExMatches();
+    if (selectedMatches) {
+      // Note that the use of selectedMatches.OrderNumber, where
+      // OrderNumber corresponds to the RegExName attribute of the Rule element
+      // in the manifest
+      $("#selected-match").text(JSON.stringify(selectedMatches.OrderNumber, null, 2));
+    } else {
+      $("#selected-match").text("Selected matches was null");
+    }
+  } else {
+    $("#selected-match").text("Method not supported on your client");
   }
 
-  function makeTableRow(name, value) {
-    return $("<tr><td><strong>" + name + 
-      "</strong></td><td class=\"prop-val\"><code>" +
-      value + "</code></td></tr>");
+  // Get all matches
+  var allMatches = Office.context.mailbox.item.getRegExMatches();
+  if (allMatches) {
+    // Note that the use of selectedMatches.OrderNumber, where
+    // OrderNumber corresponds to the RegExName attribute of the Rule element
+    // in the manifest
+    $("#all-matches").text(JSON.stringify(allMatches.OrderNumber, null, 2));
+  } else {
+    $("#all-matches").text("All matches was null");
   }
+}
 
-})();
