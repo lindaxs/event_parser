01a3b3fd5d5acb8a06336ae69a01296cd00b1789
Linda Xiao <linda.xiao.20@dartmouth.edu>
2018-04-15 11:13:38 -0400

    Modified time scanning in app.js and added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Do Something
+
+## Emma Sampugnaro, Bill Tang, Jason Wei, Linda Xiao
+
+April 2018
+
+HackDartmouthIV
+
+First:
+
+`npm install -g yo generator-office`
+
+To run:
+
+cd to event_parser directory
+then 
+`npm start`
+
+[To sideload add-in:](https://docs.microsoft.com/en-us/outlook/add-ins/sideload-outlook-add-ins-for-testing)
+
+
+
+
+
--- app.js
+++ app.js
@@ -16,12 +16,17 @@ function loadItemProps(item) {
   // Get the table body element
   var tbody = $('.prop-table');
   var body = item.body;
+  var string = "";
   
   body.getAsync(Office.CoercionType.Text, function (asyncResult) {
     if (asyncResult.status !== Office.AsyncResultStatus.Succeeded){
       console.log("Error, failed to fetch!")
     } else {
-      var string = get_details(asyncResult.value.trim());
+      string = get_details(asyncResult.value.trim());
+      var details_array = string.split(";");
+      tbody.append(makeTableRow("Time: ", details_array[0]));
+      tbody.append(makeTableRow("Date: ", details_array[1]));
+      tbody.append(makeTableRow("Location: ", details_array[2]));
     }
   });
 
@@ -30,9 +35,8 @@ function loadItemProps(item) {
   tbody.append(makeTableRow("Event:", item.subject));
   // tbody.append(makeTableRow("Message Id", item.internetMessageId));
   tbody.append(makeTableRow("With", item.from.displayName));
-  tbody.append(makeTableRow("Date: "));
-  tbody.append(makeTableRow("Time: "));
-  tbody.append(makeTableRow("Location: "));
+  // tbody.append(makeTableRow("Time: ", string));
+  console.log(string);
   
 }
 
@@ -40,34 +44,6 @@ function makeTableRow(name, value) {
   return $("<tr><td><strong>" + name + 
     "</strong></td><td class=\"prop-val\"><code>" +
     value + "</code></td></tr>");
-}
-
-function loadEntities() {
-  // getSelectedRegExMatches is in preview, so need to test for it
-  if (Office.context.mailbox.item.getSelectedRegExMatches !== undefined) {
-    var selectedMatches = Office.context.mailbox.item.getSelectedRegExMatches();
-    if (selectedMatches) {
-      // Note that the use of selectedMatches.OrderNumber, where
-      // OrderNumber corresponds to the RegExName attribute of the Rule element
-      // in the manifest
-      $("#selected-match").text(JSON.stringify(selectedMatches.OrderNumber, null, 2));
-    } else {
-      $("#selected-match").text("Selected matches was null");
-    }
-  } else {
-    $("#selected-match").text("Method not supported on your client");
-  }
-
-  // Get all matches
-  var allMatches = Office.context.mailbox.item.getRegExMatches();
-  if (allMatches) {
-    // Note that the use of selectedMatches.OrderNumber, where
-    // OrderNumber corresponds to the RegExName attribute of the Rule element
-    // in the manifest
-    $("#all-matches").text(JSON.stringify(allMatches.OrderNumber, null, 2));
-  } else {
-    $("#all-matches").text("All matches was null");
-  }
 }
 
 function get_details(content) {
@@ -79,33 +55,40 @@ function get_details(content) {
     if(hasNumber(content[i])){
       if(!hasPM(content[i])){
         if(!hasPM(content[i+1])){
-          content[i] = content[i] + 'pm';
+          if(!(hasNumber(content[i][0]) && hasNumber(content[i][1]) && hasNumber(content[i][2]))){
+            content[i] = content[i] + 'pm';
+          }
         }
       }
     }
   }
   
-  buildings = ['thayer', 'maclean', 'alpha chi', 'azd', 'gym', 'armana', 'baker', 'bartlett', 'berry', 'bildner', 'blunt', 'brace', 'burke', 'butterfield', 'byrne', 'carpenter', 'carson', 'foco', 'collis', 'xd', 'ekt', 'gile', 'goldstein', 'haldeman', 'hood', 'kd', 'kde', 'kkg', 'kemeny', 'mass', 'morton', 'murdough', 'new hamp', 'rauner', 'richardson', 'rocky', 'silsby', 'sd', 'sanborn', 'sudikoff', 'steele', 'thompson', 'topliff', 'zimmerman', 'lalacs', 'beta', 'paganucci lounge', 'sigma delt', 'fahey']
-  two_word_buildings = ['edgerton house', 'dartmouth hall', 'hanover inn', 'north fairbanks', 'one wheelock', 'st thomas']
+  
+  var buildings = ['thayer', 'maclean', 'alpha chi', 'axid', 'gym', 'armana', 'baker', 'bartlett', 'berry', 'bildner', 'blunt', 'brace', 'burke', 'butterfield', 'byrne', 'carpenter', 'carson', 'foco', 'collis', 'xd', 'ekt', 'gile', 'goldstein', 'haldeman', 'hood', 'kd', 'kde', 'kkg', 'kemeny', 'mass', 'morton', 'murdough', 'new hamp', 'rauner', 'richardson', 'rocky', 'silsby', 'sd', 'sanborn', 'sudikoff', 'steele', 'thompson', 'topliff', 'zimmerman', 'lalacs', 'beta', 'paganucci lounge', 'sigma delt', 'fahey']
+  var two_word_buildings = ['dirt cowboy', 'edgerton house', 'dartmouth hall', 'hanover inn', 'north fairbanks', 'one wheelock', 'st thomas']
   
   function get_time(content){
     var k;
+    var returned_word;
     for(k = 0; k < content.length; k++){
       var word = content[k];
+      console.log(word);
       if(word == "midnight"){
         return "11:45";
       }
       if(hasNumber(word)){
-        if(hasPM(word) || word.indexOf("-") != 0){
-          return word;
+        if(hasPM(word) && word.indexOf("-") != 0){
+           returned_word = word;
         }
         if(word.indexOf(":") != 0){
-          if(word.length < 8){
-            return word
+          if(word.length < 18){
+            return word;
           }
         }
+        
       }
     }
+    return returned_word
     
     return null
   }
@@ -134,7 +117,7 @@ function get_details(content) {
   
   function get_location(content){
     for(var i = 0; i < content.length; i++){
-      word = content[i]
+      var word = content[i]
       for(var k = 0; k < buildings.length; k ++){
         if(word.indexOf(buildings[k]) != -1){
           return word;
